b3a8cdff05e3d3d0ed93c4dda8bfd8a10c8396ce
Kyle <kdahn@bushelpowered.com>
2020-05-19 14:12:55 -0500

    Added Codelab steps

--- app/src/main/java/com/msum/csis365/androidkotlindrawernavigation/MainActivity.kt
+++ app/src/main/java/com/msum/csis365/androidkotlindrawernavigation/MainActivity.kt
@@ -8,5 +8,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // TODO - Step 6 - Add a Navigation Item Selected Listener
+
+        // TODO - Step 7 - Navigate to the Home Fragment using the Listener
     }
 }
